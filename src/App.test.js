cedd82594cc728a12e8d7b00bb9d35b345698fe5
JOSE EMILIO LABRA GAYO <labra@uniovi.es>
2020-02-27 13:25:55 +0100

    Changed tests

--- src/App.test.js
+++ src/App.test.js
@@ -4,10 +4,10 @@ import App from './App';
 
 test('add 2+2', () => {
   expect(2+2).toBe(4);
-});
+}); 
 
 /*test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Lucía/i);
   expect(linkElement).toBeInTheDocument();
-});*/
+}); */
